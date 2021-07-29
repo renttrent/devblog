ee98bf3b7d6590c1d74027daf78c164aa57e189f
renttrent <37352219+renttrent@users.noreply.github.com>
2021-07-29 19:46:10 +0200

    main page edits

--- components/header.js
+++ components/header.js
@@ -6,8 +6,8 @@ export default function Header() {
       <div className="font-mono font-extrabold text-gray-900 mx-auto text-center p-4 text-lg">
         rei(site);
       </div>
-      <div className="bg-gray-100 p-3">
-        <div className="flex flex-row justify-between w-8/12 md:w-1/2 lg:w-1/3 mx-auto text-center font-semibold">
+      <div className="bg-zima text-white p-3">
+        <div className="flex flex-row justify-between w-8/12 md:w-3/4 xl:1/3 lg:w-1/2 mx-auto text-center font-mono font-semibold">
           <Link href="/">
             <a>home</a>
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zima: "#4BBEE3",
+      },
+    },
+    fontFamily: {
+      sans: ["Lato", "sans-serif"],
+      mono: ["Space Mono", "monospace"],
+    },
   },
   variants: {
     extend: {},
