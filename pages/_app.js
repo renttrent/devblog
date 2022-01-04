95d75c07a54ea2a96cc5e6f164fabd4e60627bbd
rei <reiballa@gmail.com>
2022-01-04 01:23:17 +0100

    added head title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import Header from "../components/header";
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <> 
+      <Head>
+        <link rel="shortcut icon" href="/SVG/favicon.svg" />
+        <title>reithedev</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Header from "../components/header";
 
 export default function Home() {
   return (
