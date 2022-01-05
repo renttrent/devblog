4ff1942f160b082e344d72a4d6ba54be3505a270
rei <reiballa@gmail.com>
2022-01-05 02:13:27 +0100

    margins

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -3,7 +3,7 @@ import { getProjectsMeta } from "../../util/getter";
 
 export default function Blog({projects}) {
   return (
-    <div className="container w-2/3 mx-auto mt-4 max-w-screen-lg">
+    <div className="container w-2/3 mx-auto mt-10 max-w-screen-lg">
       {/* <div>rei(blog);</div> */}
       <div className="latest-blogs mb-9">
           <div className="font-mono text-3xl font-bold mt-4">All Projects</div>
